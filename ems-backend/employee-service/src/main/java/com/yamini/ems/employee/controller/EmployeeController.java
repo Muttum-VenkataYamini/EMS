9515af63d5d8e2cc2acb3f0b24c44bdbf64baa5e
Muttum-VenkataYamini <yaminibalaji03@gmail.com>
2026-02-15 13:53:36 +0530

    updated application.yml with dialect & also modified url

--- ems-backend/employee-service/src/main/java/com/yamini/ems/employee/controller/EmployeeController.java
+++ ems-backend/employee-service/src/main/java/com/yamini/ems/employee/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee")
 @CrossOrigin("*")
 public class EmployeeController {
 
